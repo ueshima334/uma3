116f79f8dd269d396dbf2c6ff22e4f074aea2049
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-27 23:40:45 +0900

    rubocopにの導入と修正

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,52 +1,49 @@
 class SurveysController < ApplicationController
-
-def index
-  @surveys = Survey.all.order('created_at DESC')
-end
-
-def new
-  @survey = Survey.new
-end
-
-def create
-  survey = Survey.new(survey_params)  
-
-  if survey.save
-    redirect_to surveys_path
-  else
-    @survey = Survey.new
-    render :new
+  def index
+    @surveys = Survey.all.order('created_at DESC')
   end
 
-end
+  def new
+    @survey = Survey.new
+  end
 
-def show
-  @survey = Survey.find(params[:id])
-  @vote = Vote.new
-  
-  total_vote
-end
+  def create
+    survey = Survey.new(survey_params)
 
-private
-
-def survey_params
-  params.require(:survey).permit(:title,:item1,:item2,:item3,:item4)
-end
-
-def total_vote
-
-  n = 0
-  @total_vote = []
-  
-  4.times do 
-    n += 1
-    if @survey.send("item#{n}") != ""
-   eval "@vote#{n}=Vote.where(vote_id:@survey.id,select: n )"
-   @total_vote << [ @survey.send("item#{n}") , (eval"@vote#{n}").count ]
+    if survey.save
+      redirect_to surveys_path
     else
-  break
+      @survey = Survey.new
+      render :new
     end
   end
-  @number = n
-end  
+
+  def show
+    @survey = Survey.find(params[:id])
+    @vote = Vote.new
+
+    total_vote
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:title, :item1, :item2, :item3, :item4)
+  end
+
+  def total_vote
+    @total_vote = []
+
+    4.times do |n|
+      n += 1
+      if @survey.send("item#{n}") != ''
+        eval "@vote#{n}=Vote.where(vote_id:@survey.id,select: n )"
+        @total_vote << [@survey.send("item#{n}"), (eval "@vote#{n}").count]
+      else
+        @number = n - 1
+        break
+      end
+      @number = n
+    end
+  end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,10 @@
 class VotesController < ApplicationController
-
   def create
-    vote = Vote.create(params.require(:vote).permit(:select).merge(user_id:current_user.id,vote_id:params[:survey_id]))
-     if vote.save
+    vote = Vote.create(params.require(:vote).permit(:select).merge(user_id: current_user.id, vote_id: params[:survey_id]))
+    if vote.save
       redirect_to survey_path(params[:survey_id])
-     else
+    else
       render template: 'surveys/show'
-     end
     end
-
+  end
 end
