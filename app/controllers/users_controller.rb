20aec6895a7485b8dafc0b34d6dada720c7d8140
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-28 16:30:30 +0900

    トップページに管理者用（ユーザー1）ページへ遷移するボタンを設置
    ルーティング、アクション、ビューについても記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def index
   end
 
+  def new
+  end
+
   def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 root to: "users#index"
-resources :users,only:[:show]
+resources :users,only:[:show,:new]
 resources :rooms,only:[:index]
 resources :posts do
   resources :comments,only: :create
