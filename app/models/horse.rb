f54226374766c6b61157684eb060278dee513529
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-28 16:52:57 +0900

    horse modelのバリデーションを記述

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,2 +1,11 @@
 class Horse < ApplicationRecord
+  belongs_to_active_hash :horsegender
+
+  with_options presence: { message: 'を入力してください' } do
+    validates :name
+    validates :birthday
+  end
+
+  validate :horsegender, numericality: { other_than: 1, message: 'を入力してください' } 
+
 end
